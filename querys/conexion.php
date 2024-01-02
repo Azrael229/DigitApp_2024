26297bb9d81cc0636ea75be8a8ad5cfa0b1776ff
Azrael229 <israel.navarrete229@gmail.com>
2024-01-02 07:27:19 -0600

    conexion remota

--- querys/conexion.php
+++ querys/conexion.php
@@ -1,11 +1,26 @@
 <?php
+//datos locales
 $servername = "localhost";  // Change to your database server
 $username = "root";     // Change to your database username
 $password = "";     // Change to your database password
 $database = "digitapp2024"; // Change to your database name
 
-// Create a connection
-$conexion = new mysqli($servername, $username, $password, $database);
+//datos remotos
+$servernameR = "localhost";  // Change to your database server
+$usernameR = "servico1_israelprogramador";     // Change to your database username
+$passwordR = "744920lovepass";     // Change to your database password
+$databaseR = "servico1_digitapp2024"; // Change to your database name
+
+
+
+// Create a connection local
+// $conexion = new mysqli($servername, $username, $password, $database);
+
+
+
+
+// Create a connection Remota
+$conexion = new mysqli($servernameR, $usernameR, $passwordR, $databaseR);
 
 // Check the connection
 // if ($conexion->connect_error) {
